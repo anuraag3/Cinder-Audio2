ca8458aa0eae739f211a20405794fa04ccab529e
Anuraag Agarwal <anuraag3@illinois.edu>
2020-05-07 03:08:12 +0530

    Added libraries in cmake file 3

--- proj/cmake/Cinder-Audio2Config.cmake
+++ proj/cmake/Cinder-Audio2Config.cmake
@@ -18,7 +18,7 @@ if(NOT TARGET Cinder-Audio2)
     # Notice that `cinderblock.xml` has `<includePath>src</includePath>`.
     # So you need to set `../../src/` to include.
     target_include_directories(Cinder-Audio2 PUBLIC "${Cinder-Audio2_PROJECT_ROOT}/src" )
-    target_include_directories(Cinder-Audio2 SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
+    target_include_directories(Cinder-Audio2 SYSTEM BEFORE PUBLIC "${CINDER_PATH}/src" )
     target_include_directories(Cinder-Audio2 PRIVATE
             "${Cinder-Audio2_PROJECT_ROOT}/src/cinder"
             "${Cinder-Audio2_PROJECT_ROOT}/src/oggvorbis"
